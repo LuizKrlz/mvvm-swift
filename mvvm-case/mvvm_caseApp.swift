7304427969fca4a6f98819e21af98cbcc77f1c45
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2024-09-01 09:09:51 +0100

    Initial Commit

---
+++ mvvm-case/mvvm_caseApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvm_caseApp.swift
+//  mvvm-case
+//
+//  Created by Luiz Correa on 01/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct mvvm_caseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
